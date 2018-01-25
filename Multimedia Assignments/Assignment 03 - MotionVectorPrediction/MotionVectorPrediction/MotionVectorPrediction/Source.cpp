4f9d7db9ed06c5cf50fc64dbfb3db61e4a6b8f01
WinterPu <striveant@outlook.com>
2018-01-25 22:16:43 +0800

    Add Image Files & Add [PGM] Output Function

--- Multimedia Assignments/Assignment 03 - MotionVectorPrediction/MotionVectorPrediction/MotionVectorPrediction/Source.cpp
+++ Multimedia Assignments/Assignment 03 - MotionVectorPrediction/MotionVectorPrediction/MotionVectorPrediction/Source.cpp
@@ -20,19 +20,21 @@ int main() {
 
 	Predictor predictor(16,15,reference_image,target_image);
 	predictor.Predict(Predictor::SEQUENTIAL_SEARCH);
-	std::cout << " Sequential Search:" << std::endl;
-	std::cout << " Total Consuming Time:" << predictor.GetMethodConsumingTime()<<std::endl;
-	std::cout << " SNR Value:" << predictor.GetSNRValue()<< std::endl << std::endl << std::endl;
+	std::cout << "Sequential Search:" << std::endl;
+	std::cout << "Total Consuming Time:" << predictor.GetMethodConsumingTime()<<std::endl;
+	std::cout << "SNR Value:" << predictor.GetSNRValue()<< std::endl << std::endl << std::endl;
 	cv::imshow("Sequential Search Image", predictor.GetPredictedImage());
-
-
+	std::vector<int> compression_params;//https://docs.opencv.org/3.0-beta/modules/imgcodecs/doc/reading_and_writing_images.html
+	compression_params.push_back(CV_IMWRITE_PXM_BINARY);
+	compression_params.push_back(1);
+	cv::imwrite(path_name + "Sequential Search Image.pgm", predictor.GetPredictedImage(), compression_params);
 
 	predictor.Predict(Predictor::LOGARITHM_2D_SEARCH);
-	std::cout << " 2D Logarithm Search:" << std::endl;
-	std::cout << " Total Consuming Time:" << predictor.GetMethodConsumingTime() << std::endl;
-	std::cout << " SNR Value:" << predictor.GetSNRValue() << std::endl;
-	cv::imshow(" 2D Logarithm Search Image", predictor.GetPredictedImage());
-
+	std::cout << "2D Logarithm Search:" << std::endl;
+	std::cout << "Total Consuming Time:" << predictor.GetMethodConsumingTime() << std::endl;
+	std::cout << "SNR Value:" << predictor.GetSNRValue() << std::endl;
+	cv::imshow("2D Logarithm Search Image", predictor.GetPredictedImage());
+	cv::imwrite(path_name + "2D Logarithm Search Image.pgm", predictor.GetPredictedImage(), compression_params);
 
 
 	cv::imshow("Reference Image", reference_image);
